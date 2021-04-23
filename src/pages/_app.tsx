be80706b46c9602889baf3390677b4fae86c9775
alissonph <37026901+alissonph@users.noreply.github.com>
2021-04-23 19:44:34 -0300

    Adicionado loading ao trocar de rota

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
+import Router from 'next/router';
+import NProgress from 'nprogress'
+
 import '../styles/global.scss';
+import styles from '../styles/app.module.scss'
 
 import { Header } from "../components/Header";
 import { Player } from "../components/Player";
 import { PlayerContextProvider } from '../contexts/PlayerContext';
 
-import styles from '../styles/app.module.scss'
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 function MyApp({ Component, pageProps }) {
   return (
